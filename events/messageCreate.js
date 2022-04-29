f5a4333a92738bd81b4f8f4cf2ea6b6af8e415ce
Shkiper_UASH <vovamyrosh2003@gmail.com>
2022-04-30 00:11:08 +0300

    heroku fuck you

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -37,9 +37,9 @@ const messageCreate = new Event(client, async message => {
                 rows[0].level++;
                 const console = await client.guild.channels.fetch('704660113750884433');
                 await console.send({
-                    /*content: `${member}`,*/
+                    content: `${member}`,
                     embeds: [{
-                        description: `${member} ви досягнули ${rows[0].level} рівень! Вітаєм!`,
+                        description: `Ви досягнули ${rows[0].level} рівень! Вітаєм!`,
                         color: '#2D7144'
                     }]
                 })
