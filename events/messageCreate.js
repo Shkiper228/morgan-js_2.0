edc6d07e1d86be9dbcf0bea62d7c283fa8e88f6d
Shkiper_UASH <vovamyrosh2003@gmail.com>
2022-04-29 22:41:19 +0300

    heroku fuck you

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -27,7 +27,7 @@ const messageCreate = new Event(client, async message => {
     client.connection.query(`SELECT * FROM members WHERE id = ${message.author.id}`, async (error, rows) => {
         if(rows[0]) {
             let expForNextLvl;
-            for(let i = 0; i < rows[0]; i++){
+            for(let i = 0; i < rows[0].level; i++){
                 expForNextLvl += 5 * Math.pow(rows[0].level + 1 + i, 2) + 50 * (rows[0].level + 1 + i) + 100;
             }
             const exp = rows[0].experience + Math.floor(Math.random() * 10) + 15;
