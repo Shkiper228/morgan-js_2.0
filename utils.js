e4a0d9648070cbd7e6b30bc654d5d815516e4a58
Shkiper_UASH <vovamyrosh2003@gmail.com>
2022-05-03 13:22:05 +0300

    rank card

--- utils.js
+++ utils.js
@@ -1,4 +1,5 @@
 const log = require('./classes/Logger.js');
+const { createCanvas } = require('canvas');
 
 
 async function findOrCreateChannel(client, name) {
@@ -27,3 +28,31 @@ async function findOrCreateChannel(client, name) {
 }
 
 module.exports.findOrCreateChannel = findOrCreateChannel;
+
+// function fillRectRadius(context, x, y, width, height, radius) {
+//     context.fillRect(x, y + radius, width, height - 2 * radius);
+//     context.fillRect(x + radius, y, width - 2 * radius, height);
+//     // context.beginPath();
+//     // context.moveTo(x, y + radius);
+//     // context.arcTo(x, y + height, x + radius, y + height, radius);
+//     // context.arcTo(x + width, y + height, x + width, y + height - radius, radius);
+//     // context.arcTo(x + width, y, x + width - radius, y, radius);
+//     // context.arcTo(x, y, x, y + radius, radius);
+//     // context.stroke();
+// }
+
+// module.exports.fillRectRadius = fillRectRadius;
+
+
+function fillRectRadius(context, x, y, width, height, radius) {
+    context.beginPath();
+    context.moveTo(x, y + radius);
+    context.arcTo(x, y + height, x + radius, y + height, radius);
+    context.arcTo(x + width, y + height, x + width, y + height - radius, radius);
+    context.arcTo(x + width, y, x + width - radius, y, radius);
+    context.arcTo(x, y, x, y + radius, radius);
+    context.fill();
+    //context.stroke();
+}
+
+module.exports.fillRectRadius = fillRectRadius;
