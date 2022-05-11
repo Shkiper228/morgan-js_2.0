42c65549f9b3d9452c299909585d8256af237fbb
Shkiper_UASH <vovamyrosh2003@gmail.com>
2022-05-11 23:50:52 +0300

    fix

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -4,6 +4,7 @@ const voiceStateUpdate = new Event(client, async (oldState, newState) => {
     if(newState.channelId == client.creatende_privat_voice.id) {
         const channel = await client.guild.channels.create(newState.member.user.tag, {
             type: 'GUILD_VOICE',
+            parent: newState.channel.parent,
             userLimit: 2
         })
         client.privat_voices.push(channel);
