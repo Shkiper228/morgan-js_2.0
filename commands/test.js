7cf2a5b44ee2f112afb2a3b5ceea8ef74b054312
Shkiper_UASH <vovamyrosh2003@gmail.com>
2022-05-21 13:34:52 +0300

    am

--- commands/test.js
+++ commands/test.js
@@ -1,15 +1,21 @@
-const {MessageEmbed, MessageActionRow, MessageButton} = require('discord.js');
-const cutNum = require('../utils.js').cutNum;
+const { createCanvas, loadImage } = require('canvas');
 const Command = require('../classes/Command.js');
+const { MessageAttachment } = require('discord.js')
+const { ChannelType } = require('discord.js')
 const log = require('../classes/Logger.js');
+const { fillRectRadius } = require('../utils/canvasUtils.js');
+const { groundChannel } = require('../utils/channelsUtils.js');
+const fetch = require('node-fetch');
+const { stringAndNumsFormat, checkAndConvertOfType } = require('../utils/stringAndNumsFormat.js');
+
 
 const test = new Command(client, {
     name: 'test',
-    description: 'Тестова команда. Лише для розробника',
-    ownerOnly: true,
+    description: 'Тестова команда',
+    ownerOnly: false,
     adminOnly: false
 }, async (client, message, args) => {
-    log(await client.guild.channels.fetch('704384154925662280'))
+	log(checkAndConvertOfType('1.1', 'number'));
 })
 
 module.exports = test;
