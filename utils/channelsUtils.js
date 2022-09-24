3e1b94db6456db43bc5403c63f125ea65182caad
= <=>
2022-09-25 01:20:22 +0300

    fix channel init

--- utils/channelsUtils.js
+++ utils/channelsUtils.js
@@ -45,7 +45,7 @@ async function groundChannel (client, name, data = {}, primary = true) {
             await channel.edit(data)
             return channel;
         } else {
-            channel = await client.guild.channels.create(data);
+            channel = await client.guild.channels.create({name: data.name, type: data.type});
             return channel;
         }
     //} catch (error) {
